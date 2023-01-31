e8bd9cba5b0a2e5665c5a741cd5cb6239a7796a7
Robert Lutrick <r.lutrick@icloud.com>
2023-01-31 15:53:57 -0600

    uncomment

--- client/src/views/NavBar.jsx
+++ client/src/views/NavBar.jsx
@@ -5,8 +5,8 @@ const NavBar = () => {
         <div className='border d-flex align-items-center justify-content-between p-3'>
             <h1>Health First Recipes</h1>
             <div>
-                {/* <a href="#" className='p-3'>Home</a> */}
-                {/* <a href="#" className='p-3'>Login</a> */}
+                <a href="#" className='p-3'>Home</a>
+                <a href="#" className='p-3'>Login</a>
             </div>
         </div>
     )
